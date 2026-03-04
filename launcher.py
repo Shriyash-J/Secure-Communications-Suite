edc9a111901e3b725c80dd7cbb5678e894e65dfa
Shriyash-J <joshishriyash822005@gmail.com>
2026-03-04 16:55:55 +0530

    Update launcher.py

--- launcher.py
+++ launcher.py
@@ -84,4 +84,5 @@ def launch(project_id):
 
 if __name__ == "__main__":
     print("🚀 CYBER_SUITE // COMMAND_NODE INITIALIZING...")
-    app.run(host="127.0.0.1", port=5000, debug=False)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
